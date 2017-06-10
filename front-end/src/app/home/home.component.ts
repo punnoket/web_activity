7700c26c745b949c5b0cff8c913414d9471ec15e
Punnnnn ' n <Punnnnn ' n>
2017-06-11 04:24:41 +0700

    join acc

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -17,6 +17,10 @@ export class HomeComponent implements OnInit {
 
   private objectData: any;
   private activities
+  private choices
+  private keyJoin
+  private checkJoin = true
+  private idJoin
   constructor(private loginService: LoginService, private getactivity: GetAcService) {
     this.getactivity.getActivity().subscribe(data => this.getActivityData(data))
   }
@@ -30,6 +34,42 @@ export class HomeComponent implements OnInit {
     console.log(JSON.parse(value._body));
     this.activities = JSON.parse(value._body).activity
     console.log(this.check)
+  }
+
+  public getChoices(id: String) {
+
+    for (let i of this.activities) {
+      if (id === i._id) {
+        this.choices = i.choice
+        console.log(i.name)
+      }
+    }
+  }
+
+  public join(id: String) {
+    this.idJoin = id
+    for (let i of this.activities) {
+      if (id === i._id) {
+        this.keyJoin = i.key
+        console.log(i.key)
+      }
+    }
+  }
+
+  public checkMatchKey(key: String) {
+
+    if (this.keyJoin === key) {
+      this.checkJoin = true
+      console.log("join !!")
+      this.getactivity.join(this.idJoin).subscribe(data => this.getJoinData(data))
+    } else {
+      this.checkJoin = false
+    }
+  }
+
+  private getJoinData = function(value) {
+    console.log(JSON.parse(value._body));
+
   }
 
 
